9f37186693b75befd62bdb552ee0f4e22e342969
Abhinav Mudugu <am225mq@student.lnu.se>
2025-11-29 19:35:51 +0100

    RED: addEntry should add entry to the list.

--- app/src/test/java/org/example/PasswordRepoTest.java
+++ app/src/test/java/org/example/PasswordRepoTest.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class PasswordRepoTest {
-    
+    @Test
+    public void addEntryShouldAddEntryToRepo() {
+        PasswordRepo repo = new PasswordRepo();
+        Entry entry = new Entry("Google.com", "alicia", "pass1234");
+
+        repo.addEntry(entry);
+        assertEquals(1, repo.count());
+    }
 }
