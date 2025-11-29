5a71c6ef61de355da6fc08d208afefe10bcb55d7
Abhinav Mudugu <am225mq@student.lnu.se>
2025-11-29 18:13:51 +0100

    GREEN: saved user to array

--- app/src/main/java/org/example/UserRepo.java
+++ app/src/main/java/org/example/UserRepo.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepo {
-    private User[] users;
+    private final List<User> users = new ArrayList<>();
 
     public void saveUser(User user) {
-
+        users.add(user);
     }
     public User findByUsername(String Username) {
         return null;
+    }
+
+    public int count() {
+        return users.size();
     }
 }
