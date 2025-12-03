bd75c9fb956063eb7e7f6b86727380b9dfa14ba7
Abhinav Mudugu <am225mq@student.lnu.se>
2025-12-03 12:59:10 +0100

    GREEN: encrpyt Should encypt the password

--- app/src/main/java/org/example/EncryptionService.java
+++ app/src/main/java/org/example/EncryptionService.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import javax.crypto.SecretKey;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.util.Base64;
+
 public class EncryptionService {
+    private SecretKey key;
+
+    public EncryptionService(){
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            this.key = keyGen.generateKey();
+        } catch(Exception e) {
+            throw new RuntimeException("Error generating AES key", e);
+        }
+    }
+
     public String Encrypt(String password) {
-        return null;
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encryptedBytes = cipher.doFinal(password.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public String Decrypt(String password) {
