fd66d3d231a09a3f100ae957dce7bd56fcaba734
Abhinav Mudugu <am225mq@student.lnu.se>
2025-12-03 13:16:26 +0100

    RED: encrpyt should throw id password is empty or null

--- app/src/test/java/org/example/EncryptionServiceTest.java
+++ app/src/test/java/org/example/EncryptionServiceTest.java
@@ -3,6 +3,7 @@ package org.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 
@@ -15,7 +16,28 @@ public class EncryptionServiceTest {
 
         assertNotNull(encrypted);
         assertNotEquals("pass123", encrypted);
-
     }
+
+    @Test
+    public void encrpytShouldThrowIfPasswordIsEmptyOrNull(){
+        EncryptionService service = new EncryptionService();
+
+        assertThrows(NullPointerException.class, ()->{
+            service.Encrypt(null);
+        });
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            service.Encrypt("");
+        });
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            service.Encrypt(" ");
+        });
+
+        
+        assertNotEquals("pass123 ", service.Encrypt("pass123 "));
+    }
+
+
 
 }
