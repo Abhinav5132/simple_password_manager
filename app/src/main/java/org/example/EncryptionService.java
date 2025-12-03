1880e8379c8f8ab128d466e608e3f253d767acf5
Abhinav Mudugu <am225mq@student.lnu.se>
2025-12-03 13:24:12 +0100

    GREEN: decrypt decrypts and return the encrypted string

--- app/src/main/java/org/example/EncryptionService.java
+++ app/src/main/java/org/example/EncryptionService.java
@@ -6,8 +6,6 @@ import javax.crypto.KeyGenerator;
 
 import java.util.Base64;
 
-import jdk.jshell.spi.ExecutionControl;
-
 public class EncryptionService {
     private SecretKey key;
 
@@ -41,8 +39,17 @@ public class EncryptionService {
         }
     }
 
-    public String Decrypt(String password) {
-        return null;
+    public String Decrypt(String encypted_password) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decoded = Base64.getDecoder().decode(encypted_password);
+            byte[] decryptedBytes = cipher.doFinal(decoded);
+            return new String(decryptedBytes);
+
+        } catch (Exception e) {
+            throw new RuntimeException("AES decryption failed", e);
+        }
     }
 
     public String Hash(String password) {
